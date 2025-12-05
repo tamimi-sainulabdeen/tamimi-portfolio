947f9281cd347dc03a882f6070e8974c9411f719
tamimi-sainulabdeen <tamimisainulabdeen2001@gmail.com>
2025-12-05 22:21:15 +0530

    pdf

--- src/data/portfolio-data.ts
+++ src/data/portfolio-data.ts
@@ -5,7 +5,7 @@ export const personalInfo = {
   email: "tamimi.abdeen@gmail.com",
   phone: "+971 52 103 5057",
   github: "https://github.com/tamimi-sainulabdeen",
-  linkedin: "https://linkedin.com/in/tamimisainulabdeen",
+  linkedin: "https://linkedin.com/in/tamimi-s",
   location: "Dubai, United Arab Emirates",
   behance: "behance.com"
 }
